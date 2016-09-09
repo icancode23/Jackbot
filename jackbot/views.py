7b491ff9f8a58ce7ba3b68ad060b44bac8dfe932
nipun arora <nipunarora@nipuns-MacBook-Pro-2.local>
2016-09-10 00:44:22 +0530

    customising replies

--- jackbot/views.py
+++ jackbot/views.py
@@ -12,7 +12,7 @@ def post_facebook_message(fbid,message_text):
 	post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
 	response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":message_text}})
 	status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
-	print status.json()
+	# print status.json()
 class MychatbotView(generic.View):
 	def get(self,request,*args,**kwargs):
 		if self.request.GET['hub.verify_token']==VERIFY_TOKEN:
