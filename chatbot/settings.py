030355ca061f3568ba98354b51f2f0ec23bb04f4
nipun arora <nipunarora@nipuns-MacBook-Pro-2.local>
2016-09-09 12:43:47 +0530

    customising replies

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 )
 
 MIDDLEWARE_CLASSES = (
@@ -46,6 +47,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'jackbot',
 )
 
 ROOT_URLCONF = 'chatbot.urls'
--- jackbot/views.py
+++ jackbot/views.py
@@ -34,7 +34,7 @@ class MychatbotView(generic.View):
 				print message
 				try:
 					sender_id = message['sender']['id']
-					message_text =u'this is nipun'
+					message_text ="this is me"
 					post_facebook_message(sender_id,message_text) 
 				except Exception as e:
 				 	print e
