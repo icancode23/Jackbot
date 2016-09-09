23a4610b3f83257ae16908360beed0aefda00a25
nipun arora <nipunarora@nipuns-MacBook-Pro-2.local>
2016-09-10 01:08:26 +0530

    customising replies

--- jackbot/views.py
+++ jackbot/views.py
@@ -38,7 +38,7 @@ class MychatbotView(generic.View):
 					print '*' * 8,'\n'
 					post_facebook_message(sender_id,message_text) 
 				except Exception as e:
-				 	print e
+				 	#print e
 					pass
 
 		return HttpResponse()  
