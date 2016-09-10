5448f4ecb92eb98f58c0e40e8b97906cf51d0cbf
nipun arora <nipunarora@nipuns-MacBook-Pro-2.local>
2016-09-10 16:43:51 +0530

    customising replies

--- jackbot/views.py
+++ jackbot/views.py
@@ -21,8 +21,6 @@ class MychatbotView(generic.View):
 		else :
 			return HttpResponse('oops invalid session')
 	
-	global requestcount
-	requestcount=1
 	@method_decorator(csrf_exempt)
 	def dispatch(self,request,*args,**kwargs):
 		return generic.View.dispatch(self,request,*args,**kwargs)
@@ -30,11 +28,11 @@ class MychatbotView(generic.View):
 		incoming_message=json.loads(self.request.body.decode('utf-8'))
 		print 'the incoming message is ',incoming_message
 		print 'the type of incoming message is',type(incoming_message)
-		print  ('text' in incoming_message['entry'][0]['messaging'][0]['message'].keys() and  incoming_message['entry'][0]['messaging'][0]['recipient']['id']=='304882153210685')
-		if ('text' in incoming_message['entry'][0]['messaging'][0]['message'].keys() and  incoming_message['entry'][0]['messaging'][0]['recipient']['id']=='304882153210685'):
-			global messagelist
-			messagelist=['this is me nipun huh']
-			print messagelist
+		# print  ('text' in incoming_message['entry'][0]['messaging'][0]['message'].keys() and  incoming_message['entry'][0]['messaging'][0]['recipient']['id']=='304882153210685')
+		# # if ('text' in incoming_message['entry'][0]['messaging'][0]['message'].keys() and  incoming_message['entry'][0]['messaging'][0]['recipient']['id']=='304882153210685'):
+		# # 	global messagelist
+		# # 	messagelist=['this is me nipun huh']
+		# # 	print messagelist
 		for entry in incoming_message['entry']:
 			for message in entry['messaging']:
 				print 'the RECIPIENT id is',message['recipient']['id']
