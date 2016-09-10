d12253938b2ac7df51847726bf43aeba5a544301
nipun arora <nipunarora@nipuns-MacBook-Pro-2.local>
2016-09-10 15:02:42 +0530

    customising replies

--- jackbot/views.py
+++ jackbot/views.py
@@ -36,8 +36,9 @@ class MychatbotView(generic.View):
 		 		global messagelist
 		 		messagelist=['so who are you? huh']
 		 		print messagelist
-		 # 		try:
-		 # 			requestcount=requestcount+1
+		  		try:
+		  			requestcount=requestcount+1
+		  			print requestcount
 		 # 		except Exception as es:
 		 # 			print "the error is",es
 			# else:
