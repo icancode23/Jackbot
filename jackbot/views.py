334d0f2b8d924d621e52f414dfdb618715295e95
nipun arora <nipunarora@nipuns-MacBook-Pro-2.local>
2016-09-10 16:40:24 +0530

    customising replies

--- jackbot/views.py
+++ jackbot/views.py
@@ -23,7 +23,6 @@ class MychatbotView(generic.View):
 	
 	global requestcount
 	requestcount=1
-	global messagelist
 	@method_decorator(csrf_exempt)
 	def dispatch(self,request,*args,**kwargs):
 		return generic.View.dispatch(self,request,*args,**kwargs)
@@ -33,6 +32,7 @@ class MychatbotView(generic.View):
 		print 'the type of incoming message is',type(incoming_message)
 		print  ('text' in incoming_message['entry'][0]['messaging'][0]['message'].keys() and  incoming_message['entry'][0]['messaging'][0]['recipient']['id']=='304882153210685')
 		if ('text' in incoming_message['entry'][0]['messaging'][0]['message'].keys() and  incoming_message['entry'][0]['messaging'][0]['recipient']['id']=='304882153210685'):
+			global messagelist
 			messagelist=['this is me nipun huh']
 			print messagelist
 		for entry in incoming_message['entry']:
