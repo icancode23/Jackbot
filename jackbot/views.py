832b2c713ee486ad21098cda0d368acf3fc23b0f
nipun arora <nipunarora@nipuns-MacBook-Pro-2.local>
2016-09-10 15:57:55 +0530

    customising replies

--- jackbot/views.py
+++ jackbot/views.py
@@ -40,13 +40,14 @@ class MychatbotView(generic.View):
 					sender_id = message['sender']['id']
 					#print 'the sender id is working'
 					#message_text=message['message']['text']
+					print messagelist[0]
 					message_text =messagelist[0]
 					messagelist.pop()
 					# print '*' * 8
 					# print 'this is the message text',message_text
 					post_facebook_message(sender_id,message_text) 
 				except Exception as e:
-				 	#print e
+				 	print e
 				 	print 'this is an error'
 					pass
 
