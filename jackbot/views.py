5d7fac18e7de8d38f2112f1ce26806a27ba63374
nipun arora <nipunarora@nipuns-MacBook-Pro-2.local>
2016-09-10 19:22:25 +0530

    customising replies

--- jackbot/views.py
+++ jackbot/views.py
@@ -27,23 +27,23 @@ class MychatbotView(generic.View):
 		return generic.View.dispatch(self,request,*args,**kwargs)
 	def post (self,request,*args,**kwargs):
 		incoming_message=json.loads(self.request.body.decode('utf-8'))
-		print 'the incoming message is ',incoming_message
-		print 'the type of incoming message is',type(incoming_message)
-		print  ('text' in incoming_message['entry'][0]['messaging'][0]['message'].keys() and  incoming_message['entry'][0]['messaging'][0]['recipient']['id']==304882153210685)
-		if ('text' in incoming_message['entry'][0]['messaging'][0]['message'].keys() and  incoming_message['entry'][0]['messaging'][0]['recipient']['id']==304882153210685):
-			global messagelist
-			messagelist=['this is the message i am talking about']
-			print messagelist
+		# print 'the incoming message is ',incoming_message
+		# print 'the type of incoming message is',type(incoming_message)
+		# print  ('text' in incoming_message['entry'][0]['messaging'][0]['message'].keys() and  incoming_message['entry'][0]['messaging'][0]['recipient']['id']==304882153210685)
+		# if ('text' in incoming_message['entry'][0]['messaging'][0]['message'].keys() and  incoming_message['entry'][0]['messaging'][0]['recipient']['id']==304882153210685):
+		# 	global messagelist
+		# 	messagelist=['this is the message i am talking about']
+		# 	print messagelist
 		for entry in incoming_message['entry']:
 			for message in entry['messaging']:
 				print 'the RECIPIENT id is',message['recipient']['id']
 				try:
 					sender_id=message['sender']['id']
 					#print 'the sender id is working'
-				#	message_text=message['message']['text']
+					message_text=message['message']['text']
 					#print messagelist[0]
-					message_text=messagelist[0]
-					messagelist.pop()
+					# message_text=messagelist[0]
+					# messagelist.pop()
 					# print '*' * 8
 					# print 'this is the message text',message_text
 					post_facebook_message(sender_id,message_text) 
