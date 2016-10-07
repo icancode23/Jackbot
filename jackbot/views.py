17a5ac516f87c309de6d51a4db71135d7f792704
nipun arora <nipunarora@nipuns-MacBook-Pro-2.local>
2016-10-07 20:58:40 +0530

    hey theremad

--- jackbot/views.py
+++ jackbot/views.py
@@ -18,7 +18,7 @@ def post_facebook_message(fbid,message_text):
 
 	post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN#fb api to send messages to a particular fb handle 
 	response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":message_text}})
-	status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+	status = requests.post(post_message_url, her,data=response_msg)
 	# print status.json()
 class MychatbotView(generic.View):
 	def get(self,request,*args,**kwargs):
